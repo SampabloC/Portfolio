be7b169cf32c42c9c355780371bde42a61d640d0
SampabloC <sampablo-jisc@outlook.com>
2023-05-18 09:27:56 -0600

    Install dependencies and prepare enviroment Install react-scroll, node-sass, react-icons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero';
+import Contact from './components/Contact';
+import Profile from './components/Profile';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar />
+    <Hero />
+    <Profile />
+    <Projects />
+    <Contact />
+    <Footer />
+    </>
   );
 }
 
