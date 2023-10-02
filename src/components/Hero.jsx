52a5aa35987003b25c2211817059eea8d4931454
SampabloC <sampablo-jisc@outlook.com>
2023-10-01 23:07:53 -0600

    Modify Landing page Change landing message, add icons to technologies and redesign landing page.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,43 @@
 import React from "react";
 import '../stylesheets/hero.scss';
-import { FaArrowDown, FaRegHandSpock } from 'react-icons/fa';
+import {FaArrowDown} from 'react-icons/fa';
+import {RailsIcon,
+        RubyIcon,
+        PythonIcon,
+        DockerIcon,
+        JavascriptIcon
+        } from '../icons/Icons';
 
 export default function Hero() {
     return (
         <section id="hero">
-            <div className="landing_message">
-                <p id="title">Hello, I'm Javier Sampablo <FaRegHandSpock /></p>
-                <p id="message">A FullStack Developer with background in:</p>
+            <div className="title">
+                <p>Hi!, I'm</p>
+                <p id="name">Javier Sampablo</p>
+                <p>A Software Engineer</p>
             </div>
+            <div className="landing_message">
+                <p>I'm a passionate FullStack Web Developer
+                    with background in:
+                </p>
+            </div>
+            <section className="icons_section">
+                <a href="https://www.ruby-lang.org/en/" target="_blank" rel="noopener noreferrer">
+                    <img src={RubyIcon} alt="Ruby" />
+                </a>
+                <a href="https://rubyonrails.org/" target="_blank" rel="noopener noreferrer">
+                    <img src={RailsIcon} alt="Rails" />
+                </a>
+                <a href="https://www.python.org/" target="_blank" rel="noopener noreferrer">
+                    <img src={PythonIcon} alt="Python" />
+                </a>
+                <a href="https://www.docker.com/" target="_blank" rel="noopener noreferrer">
+                    <img src={DockerIcon} alt="Docker" />
+                </a>
+                <a href="https://www.javascript.com/" target="_blank" rel="noopener noreferrer">
+                    <img src={JavascriptIcon} alt="Javascript" />
+                </a>
+            </section>
             <div><FaArrowDown id="arrow_down"/></div>
         </section>
     )
---
+++ src/icons/Icons.jsx
@@ -0,0 +1,12 @@
+import RailsIcon from './rails-icon.svg';
+import RubyIcon from './ruby-icon.svg';
+import PythonIcon from './python-icon.svg';
+import DockerIcon from './docker-icon.svg';
+import JavascriptIcon from './javascript-icon.svg';
+
+export {RailsIcon,
+        RubyIcon,
+        PythonIcon,
+        DockerIcon,
+        JavascriptIcon
+        };
